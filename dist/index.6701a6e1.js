204517ca0b0d636ee53a8c14990b9057a6e9c1ca
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-17 13:42:20 +0200

    Update index.6701a6e1.js

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22907,7 +22907,7 @@ class MainView extends _reactDefault.default.Component {
         };
     }
     componentDidMount() {
-        _axiosDefault.default.get('https://movie_api.herokuapp.com/movies').then((response)=>{
+        _axiosDefault.default.get('https://agile-crag-85270.herokuapp.com/movies').then((response)=>{
             this.setState({
                 movies: response.data
             });
