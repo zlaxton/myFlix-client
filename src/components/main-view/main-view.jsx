6a2536cd79f12c3620554240f58e2b87033feae6
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-20 20:44:10 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -24,13 +26,22 @@ constructor(){
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: movie
     });
   }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
     const { movies, selectedMovie } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
   
     if (movies.length === 0) return <div className="main-view" />;
   
