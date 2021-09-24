546a3a2f9ffd13951865239a0caa13fef8c1518e
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-24 15:33:50 +0200

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
@@ -9,46 +13,47 @@ export function RegistrationView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password, email, birthday);
+    props.onRegistration(username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <label>
-        Create Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Birthday:
-          <input
-            type="date"
-            value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
-          />
-        </label>
-      </label>
-      <button type="submit" onClick={handleSubmit}>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-        Submit
-      </button>
-    </form>
+    <Row className="register-wrapper justify-content-md-center">
+     <Col xs={12} md={8} lg={6}>
+    <Form className="register-form">
+
+      <Form.Group>
+      <Form.Label className="username">Username:
+      <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </Form.Label>
+      <Form.Label className="name">Name:
+      <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </Form.Label>
+      <Form.Label className="password">Password:
+      <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </Form.Label>
+      <Form.Label className="email">E-mail:
+      <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </Form.Label>
+      <Form.Label className="birthday">Birth date:
+      <Form.Control type="date" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+      </Form.Label>
+      </Form.Group>
+      
+    
+      <button className="registerBtn" type="submit" onClick={handleSubmit}>Register </button>
+      </Form>
+      </Col>
+      </Row>
+       
   );
 }
+
+RegistrationView.propTypes = {
+  register: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthday: PropTypes.string.isRequired,
+  }),
+  onRegistration: PropTypes.func.isRequired,
+};
