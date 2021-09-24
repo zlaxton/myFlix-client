060baf1ae490b5a7ed0ec5fa4fa7dd32afd63290
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-24 14:50:36 +0200

    Update button.jsx

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,1 +1,11 @@
 import './button.scss';
+import Button from 'react-bootstrap/Button';
+
+export function Button({label}) {
+
+    return ( 
+    <Button variant="outline-primary">Primary
+      <button className="super-button">{label}</button>;
+    </Button>
+    );   
+  }
