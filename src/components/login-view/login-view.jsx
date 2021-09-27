33938df26af3928901d15d33420c2a6b7de8ef48
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-27 13:32:15 +0200

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -14,7 +14,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post('YOUR_API_URL/login', {
+    axios.post('https://rocky-bayou-72593.herokuapp.com/login', {
       Username: username,
       Password: password
     })
