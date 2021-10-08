4786168ee7f244bdf05067985055574e7cd32e93
zlaxton <zlatan.kopic171@yahoo.com>
2021-10-08 16:08:59 +0200

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -30,16 +30,18 @@ export class MovieCard extends React.Component {
       <Container>
       <Card>
         <Link to={`/movies/${movie._id}`}>
-        <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Img className="image-container" variant="top" src={movie.ImageURL} />
         </Link>
         <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Title><h4>{movie.Title}</h4></Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
-            <Button variant="link">Open</Button>
+            <Button variant="primary">Open</Button>
           </Link>
         </Card.Body>
       </Card>
+  
+
       </Container>
     );
   }
