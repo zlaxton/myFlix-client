bd34e546f8429a004dfbf7cafaeaef32af68dce3
zlaxton <zlatan.kopic171@yahoo.com>
2021-12-19 13:01:03 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,14 +8,18 @@ import MainView from './components/main-view/main-view';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 
-// Import statement to indicate that you need to bundle `./index.scss`
+/**
+ * Import statement to indicate that you need to bundle `./index.scss`
+ */
 import './index.scss';
 
 const store = createStore(moviesApp, devToolsEnhancer());
 
 
 
-// Main component (will eventually use all the others)
+/**
+ * Main component (will eventually use all the others)
+ */
 class MyFlixApplication extends React.Component {
   render() {
     return(
@@ -27,8 +31,12 @@ class MyFlixApplication extends React.Component {
     );
   }
 }
-// Finds the root of your app
+/**
+ * Finds the root of your app
+ */
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
+/**
+ * Tells React to render your app in the root DOM element
+ */
 ReactDOM.render(React.createElement(MyFlixApplication), container);
