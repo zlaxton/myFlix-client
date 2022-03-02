d475c8970880d56982080b254bce60cf35c8586a
zlaxton <zlatan.kopic171@yahoo.com>
2022-03-02 17:46:25 +0100

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,6 @@
 export const SET_MOVIES = "SET_MOVIES";
 export const SET_FILTER = "SET_FILTER";
-export const SET_USER = "SET_USER";
+export const SET_USER_PROFILE = "SET_USER_PROFILE";
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -10,6 +10,6 @@ export function setFilter(value) {
   return { type: SET_FILTER, value };
 }
 
-export function setUser(value) {
-  return { type: SET_USER, value };
+export function setUserProfile(value) {
+  return { type: SET_USER_PROFILE, value };
 }
