faf90e136995ccd872254a8de64b7cd4dbed7930
zlaxton <zlatan.kopic171@yahoo.com>
2022-03-02 17:46:06 +0100

    Update genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,41 +1,77 @@
 import React from 'react';
-//import propTypes from 'prop-types';
-import {Button} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import './genre-view.scss';
 
+import { Link } from 'react-router-dom';
 
 
 export class GenreView extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
 
   render() {
-    const { genre, onBackClick } = this.props;
+    const { genre, movies } = this.props;
+
+    if (!genre) return null;
 
     return (
       <div className="genre-view">
-
-        <div className="genre-name">
-          <h1>
-            <span className="value">{genre.Name}</span>
-          </h1>
-
-        </div>
-        <div className="genre-description">
-          <span className="value">{genre.Description}</span>
-        </div>
-
-        <Button variant="primary" onClick={() => 
-          { onBackClick(null); }}>Back</Button>
-
-      </div>
+        <Container>
+          <Card style={{ minwidth: '20rem' }} className="border-0 pl-0">
+            <Card.Body>
+              <span className="d-flex align-items-center mb-4">
+                <Link to="" onClick={() => history.back()}>
+                  <i className="material-icons">arrow_back_ios</i>
+                </Link>
+                <h1 className="display-4">{genre.Name}</h1>
+              </span>
+              <Card.Text>{genre.Description}</Card.Text>
+            </Card.Body>
+          </Card>
+          <Container>
+            <h4 className="mt-4">Some {genre.Name} movies</h4>
+            <div className="d-flex row mt-3 ml-1">
+              {movies.map(movie => {
+                if (movie.Genre.Name === genre.Name) {
+                  return (
+                    <div key={movie._id}>
+                      <Card className="mb-3 mr-2 h-100" style={{ width: '16rem' }} >
+                        <Card.Img variant="top" src={movie.ImagePath} />
+                        <Card.Body>
+                          <Link className="text-muted" to={`/movies/${movie._id}`}>
+                            <Card.Title>{movie.Title}</Card.Title>
+                          </Link>
+                          <Card.Text>{movie.Description.substring(0, 90)}...</Card.Text>
+                        </Card.Body>
+                        <Card.Footer className="bg-white border-top-0">
+                          <Link to={`/movies/${movie._id}`}>
+                            <Button variant="link" className="read-more-link pl-0">Read more</Button>
+                          </Link>
+                        </Card.Footer>
+                      </Card>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </Container>
+        </Container>
+      </div >
     );
+
   }
+
 }
 
-
-/*GenreView.propTypes = {
-  genre: propTypes.shape({
-    Name: propTypes.string.isRequired,
-    Description: propTypes.string.isRequired
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string,
+    Description: PropTypes.string
   }).isRequired
-};*/
-
-export default GenreView;
+};
