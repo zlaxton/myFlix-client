0e823e4a83613232471a987dbf33ba36b867b79f
zlaxton <zlatan.kopic171@yahoo.com>
2022-03-02 17:43:42 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import moviesApp from './reducers/reducers';
+
 import MainView from './components/main-view/main-view';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
 
-
-/**
- * Import statement to indicate that you need to bundle `./index.scss`
- */
+// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-const store = createStore(moviesApp, devToolsEnhancer());
+const store = createStore(moviesApp);
 
-
-
-/**
- * Main component (will eventually use all the others)
- */
+// Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return(
-     <Provider store={store}>
-        <Container>
-          <MainView />
-        </Container>
+    return (
+      <Provider store={store}>
+        <MainView />
       </Provider>
     );
   }
 }
-/**
- * Finds the root of your app
- */
+
+// Finds the root of the app
 const container = document.getElementsByClassName('app-container')[0];
 
-/**
- * Tells React to render your app in the root DOM element
- */
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+// Tells React to render the app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container); 
