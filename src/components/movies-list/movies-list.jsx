df52b2a5f1a727eac9e1af49f464a3303f9371c7
zlaxton <zlatan.kopic171@yahoo.com>
2022-03-02 17:44:59 +0100

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import { MovieCard } from '../movie-card/movie-card';
+import './movies-list.scss';
 
 const mapStateToProps = state => {
   const { visibilityFilter } = state;
@@ -18,18 +23,47 @@ function MoviesList(props) {
     filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
   }
 
-  if (!movies) return <div className="main-view"/>;
+  if (!movies) return <div className="main-view" />;
 
-  return <>
-    <Col md={12} style={{ margin: '1em' }}>
+  return <div className="movies-list">
+    <Row>
       <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-    </Col>
-    {filteredMovies.map(m => (
-      <Col md={3} key={m._id}>
-        <MovieCard movie={m} />
-      </Col>
-    ))}
-  </>;
+    </Row>
+
+    <Row>
+      {filteredMovies.map(m =>
+        <Row>
+          <Col key={m._id} xs={12} sm={6} md={4} lg={3} className="card-margin">
+            <MovieCard key={m._id} movie={m} />
+          </Col>
+        </Row>)}
+    </Row>
+  </div>;
+
 }
 
 export default connect(mapStateToProps)(MoviesList);
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string,
+      Title: PropTypes.string,
+      ReleaseYear: PropTypes.string,
+      ImagePath: PropTypes.string,
+      Description: PropTypes.string,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string,
+        Description: PropTypes.string
+      }),
+      Director: PropTypes.shape({
+        Name: PropTypes.string,
+        Bio: PropTypes.string,
+        Birth: PropTypes.string,
+        Death: PropTypes.string
+      }),
+      Featured: PropTypes.boolean,
+      Actors: PropTypes.array
+    })
+  ).isRequired
+};
