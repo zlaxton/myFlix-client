5d9a4064c14565f928d9725cc79f7993d467ef93
zlaxton <zlatan.kopic171@yahoo.com>
2022-03-02 17:45:14 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,93 +1,116 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-//import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Media from 'react-bootstrap/Media';
 import Button from 'react-bootstrap/Button';
+import './movie-view.scss';
+
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export class MovieView extends React.Component {
 
-  
+  constructor() {
+    super();
 
-  keypressCallback(event) {
-    console.log(event.key);
+    this.state = {};
   }
 
-  componentDidMount() {
-    document.addEventListener('keypress', this.keypressCallback);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keypress', this.keypressCallback);
-  }
-
-  addFavorite() {
-    const token = localStorage.getItem('token');
-    const username = localStorage.getItem('user');
-
-    axios.post(`https://rocky-bayou-72593.herokuapp.com/users/${username}/movies/${this.props.movie._id}`, {}, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-        alert(`Added to Favorites List`)
+  addToFavorites(e) {
+    const { movie } = this.props;
+    e.preventDefault();
+    axios.post(
+      `https://rocky-bayou-72593.herokuapp.com/users/${localStorage.getItem('user')}/Movies/${movie._id}`,
+      { username: localStorage.getItem('user') },
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
       })
-      .catch(function (error) {
-        console.log(error);
+      .then(res => {
+        alert(`${movie.Title} successfully added to your favorites`);
+      })
+      // .then(res => {
+      //   window.open(`/users/${localStorage.getItem('user')}`)
+      // })
+      .then(res => {
+        document.location.reload(true);
+      })
+      .catch(error => {
+        alert(`${movie.Title} not added to your favorites` + error)
       });
-  };
+  }
+
 
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movie } = this.props;
+
+    if (!movie) return null;
+
+
 
     return (
       <div className="movie-view">
+        <Media className="d-flex flex-column flex-md-row align-items-center ml-xs-5">
+          <Media.Body>
+            <span className="d-flex align-items-center">
+              <Link to={`/`}>
+                <i className="material-icons arrow-back">arrow_back_ios</i>
+              </Link>
+              <h1 className="display-4">{movie.Title}</h1>
+            </span>
+            <span className="d-flex align-items-center">
+              <h4 className="text-muted mt-4 mr-2">Genre: </h4>
+              <Link to={`/genres/${movie.Genre.Name}`}>
+                <h4 className="genre-link link mt-4">{movie.Genre.Name}</h4>
+              </Link>
+            </span>
+            <span className="d-flex align-items-center mb-4">
+              <h4 className="text-muted mr-2">Director: </h4>
+              <Link to={`/directors/${movie.Director.Name}`}>
+                <h4 className="director-link link">{movie.Director.Name}</h4>
+              </Link>
+            </span>
 
-        <div className="movie-poster">
-          <img src={movie.ImageURL} />
-        </div>
-        
-        <div className="movie-title">
-          <h1 bg="primary">
-              <span className="value">{movie.Title}</span>
-          </h1>
-        </div>
 
-        <div className="movie-description">
-          <span className="value">{movie.Description}</span>
-        </div>
+            <p className="text-justify">{movie.Description}</p>
+          </Media.Body>
+          <img
+            width={220}
+            height={326}
+            className="ml-3"
+            src={movie.ImagePath}
+            alt="movie-poster placeholder"
+          />
+        </Media>
+        <Button className="add-favorite-btn mt-4" onClick={e => this.addToFavorites(e)}>
+          <span className="d-flex align-items-center">
+            <i className="material-icons heart mr-3">favorite</i>
+            Add to my favorites
+              </span>
 
-        <div className="movie-genre button-space">
-        <Link to={`/genres/${movie.Genre.Name}`}>Genre: </Link>    
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-
-        <div className="movie-director button-space">
-        <Link to={`/directors/${movie.Director.Name}`}>Director: </Link>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-
-        <Button variant='sucess' className="fav-button" value={movie._id} onClick={(e) => 
-          this.addFavorite(e, movie)}>
-          Add to Favorites
         </Button>
-
-        <div className="button-space"></div>
-        <Button variant="primary" onClick={() => 
-          { onBackClick(null); }}>Back</Button>
-      </div>
+      </div >
     );
   }
 }
-/*MovieView.propTypes = {
-  movie: propTypes.shape({
-    Title: propTypes.string.isRequired,
-    Description: propTypes.string.isRequired,
-    ImageURL: propTypes.string.isRequired,
-    Featured: propTypes.bool,
-    Genre: propTypes.shape({
-      Name: propTypes.string.isRequired
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string,
+    Title: PropTypes.string,
+    ReleaseYear: PropTypes.string,
+    ImagePath: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
     }),
-    Director: propTypes.shape({
-      Name: propTypes.string.isRequired
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
     }),
-  }).isRequired
-};*/
+    Featured: PropTypes.boolean,
+    Actors: PropTypes.array
+  })
+    .isRequired
+};
